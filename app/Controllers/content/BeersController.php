e0f2d7046e0cfd9114fb009576f4c2699749c489
cdbschr <c.debusscher@gmail.com>
2022-05-05 14:24:10 +0200

    👌 IMPROVE: beers controller CRUD

--- app/Controllers/content/BeersController.php
+++ app/Controllers/content/BeersController.php
@@ -6,12 +6,45 @@ use Beear\Controllers\Controller;
 
 Class BeersController extends Controller {
 
-	//affiche une bière dans une page unique en récupérant son id
+	//show a beer by id in a view
 	public function beerPage($data): void {
     $id = $data['id'];
     $beers = new \Beear\Models\content\BeersModel($id);
     $beer = $beers->getBeerById($id);
 
-    include $this->viewFrontend('/beers/beer');
+    include $this->viewFrontend('beer');
 	}
+
+  // show three last beers for the home page
+  public function threeLastBeers($data): void {
+    $id = $data['id'];
+    $beers = new \Beear\Models\content\BeersModel($id);
+    $threeLastBeers = $beers->getThreeLastBeers();
+
+    include $this->viewFrontend('home');
+  }
+
+  //create a beer
+  public function createBeer($data): void {
+    $beers = new \Beear\Models\content\BeersModel($data);
+    $beers->createBeer($data);
+
+    $this->viewAdmin('/Beers');
+  }
+
+  //udpate a beer
+  public function updateBeer($data): void {
+    $beers = new \Beear\Models\content\BeersModel($data);
+    $beers->updateBeer($data);
+
+    $this->viewAdmin('/Beers');
+  }
+
+  //delete a beer
+  public function deleteBeer($data): void {
+    $beers = new \Beear\Models\content\BeersModel($data);
+    $beers->deleteBeer($data);
+
+    $this->viewAdmin('/Beers');
+  }
 }
