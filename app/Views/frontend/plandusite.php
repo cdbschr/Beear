212090a7e268860493585b314d9be085cf12ef1c
cdbschr <c.debusscher@gmail.com>
2022-06-14 09:41:56 +0200

    📦 NEW: sitemap

--- app/Views/frontend/plandusite.php
+++ app/Views/frontend/plandusite.php
@@ -1,3 +1,20 @@
 <?php require_once './app/Views/frontend/layouts/head.php'; ?>
 
+<main id="sitemap" class="container">
+<h1>Plan du site</h1>
+<ol>
+	<li><a href="/">Accueil</a></li>
+	<li><a href="/#bblonde">Produits</a></li>
+	<ol>
+		<li><a href="/?action=beer-page&id=1">Beear Blonde</a></li>
+		<li><a href="/?action=beer-page&id=1">Beear Brune</a></li>
+		<li><a href="/?action=beer-page&id=1">Beear Blanche</a></li>
+	</ol>
+	<li><a href="/?action=valeurs">Valeurs</a></li>
+	<li><a href="/?action=contact">Contact</a></li>
+	<li><a href="/?action=mentions-legales">Mentions légales</a></li>
+	<li><a href="/?action=rgpd">RGPD</a></li>
+	<li><a href="/?action=plan-site">Plan du site</a></li>
+</ol>
+</main>
 <?php require_once './app/Views/frontend/layouts/footer.php'; ?>
